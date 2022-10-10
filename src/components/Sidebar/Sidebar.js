f49d97866de2ebdf5c8dd8a30b8f5218c327e143
Christian Luciani <luciani_christian@yahoo.com>
2022-10-10 17:11:59 -0500

    Build out sidebar, in progress

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,3 +1,4 @@
+import { faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Link, NavLink } from 'react-router-dom';
@@ -10,16 +11,33 @@ export default function Sidebar() {
                 <p>Christian Luciani</p>
             </Link>
             <nav>
-                <NavLink exact="true" activeclassname="active" to="/">
-                    <FontAwesomeIcon icon={faHome} color="black"/>
+                <NavLink exact="true" activeclassname="active" to="/" className="home-link">
+                    <FontAwesomeIcon icon={faHome} color="#4e4e4e"/>
                 </NavLink>
                 <NavLink exact="true" activeclassname="active" to="/about" className="about-link">
-                    <FontAwesomeIcon icon={faUser} color="black"/>
+                    <FontAwesomeIcon icon={faUser} color="#4e4e4e"/>
                 </NavLink>
                 <NavLink exact="true" activeclassname="active" to="/contact" className="contact-link">
-                    <FontAwesomeIcon icon={faEnvelope} color="black"/>
+                    <FontAwesomeIcon icon={faEnvelope} color="#4e4e4e"/>
                 </NavLink>
             </nav>
+            <ul>
+                <li>
+                    <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/christian-luciani/">
+                        <FontAwesomeIcon icon={faLinkedin} color="4e4e4e"/>
+                    </a>
+                </li>
+                <li>
+                    <a target="_blank" rel="noreferrer" href="https://github.com/LucianiChristian">
+                        <FontAwesomeIcon icon={faGithub} color="4e4e4e"/>
+                    </a>
+                </li>
+                <li>
+                    <a target="_blank" rel="noreferrer" href="https://www.instagram.com/christian_luciani/">
+                        <FontAwesomeIcon icon={faInstagram} color="4e4e4e"/>
+                    </a>
+                </li>
+            </ul>
         </div>
     )
 }
