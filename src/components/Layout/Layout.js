c144be7759949a079baa11f937a59b91b3caad04
Christian Luciani <luciani_christian@yahoo.com>
2022-10-10 14:42:41 -0500

    Begin basic routes and components

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,5 +2,9 @@ import './Layout.scss';
 import Sidebar from '../Sidebar/Sidebar.js';
 
 export default function Layout() {
-    return <><Sidebar /></>;
+    return (
+        <div className="container">
+            <Sidebar />
+        </div>
+    );
 }
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,5 +1,25 @@
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link, NavLink } from 'react-router-dom';
 import './Sidebar.scss';
 
 export default function Sidebar() {
-    return <>Hello from sidebar</>
+    return (
+        <div className="sidebar">
+            <Link className="logo" to="/">
+                <p>Christian Luciani</p>
+            </Link>
+            <nav>
+                <NavLink exact="true" activeclassname="active" to="/">
+                    <FontAwesomeIcon icon={faHome} color="black"/>
+                </NavLink>
+                <NavLink exact="true" activeclassname="active" to="/about" className="about-link">
+                    <FontAwesomeIcon icon={faUser} color="black"/>
+                </NavLink>
+                <NavLink exact="true" activeclassname="active" to="/contact" className="contact-link">
+                    <FontAwesomeIcon icon={faEnvelope} color="black"/>
+                </NavLink>
+            </nav>
+        </div>
+    )
 }
