ca04a30bc9e1f42f8fbb1dbaf55d008a41dcc780
Christian Luciani <luciani_christian@yahoo.com>
2022-10-13 20:40:37 -0500

    Fix root NavLink staying active

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -18,16 +18,16 @@ export default function Sidebar() {
                 <p className="logo">Christian Luciani</p>
             </Link>
             <nav className={showMenu ? "show" : "hide"}>
-                <NavLink exact="true" activeclassname="active" to="/" className="home-link">
+                <NavLink exact={true} activeclassname="active" to="/" end className="home-link">
                     <FontAwesomeIcon icon={faHome} color="#787692"/>
                 </NavLink>
-                <NavLink exact="true" activeclassname="active" to="/works" className="works-link">
+                <NavLink exact={true} activeclassname="active" to="/works" className="works-link">
                     <FontAwesomeIcon icon={faBriefcase} color="#787692"/>
                 </NavLink>
-                <NavLink exact="true" activeclassname="active" to="/posts" className="posts-link">
+                <NavLink exact={true} activeclassname="active" to="/posts" className="posts-link">
                     <FontAwesomeIcon icon={faPen} color="#787692"/>
                 </NavLink>
-                <NavLink exact="true" activeclassname="active" to="/contact" className="contact-link">
+                <NavLink exact={true} activeclassname="active" to="/contact" className="contact-link">
                     <FontAwesomeIcon icon={faEnvelope} color="#787692"/>
                 </NavLink>
             </nav>
