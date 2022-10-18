025f8520bcc011c21fa8f06894e88e5dbd7c613f
Christian Luciani <luciani_christian@yahoo.com>
2022-10-18 07:51:53 -0500

    Modify Routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import Layout from './components/Layout/Layout.js';
-import Home from './components/Home/Home.js';
-import Works from './components/Works/Works.js';
-import Posts from './components/Posts/Posts.js';
-import Contact from './components/Contact/Contact.js';
 import './App.scss';
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="works" element={<Works />} />
-          <Route path="posts" element={<Posts />} />
-          <Route path="contact" element={<Contact />} />
-        </Route>
-      </Routes>
-    </>
+    <Layout />
   );
 }
 
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,23 @@
 import './Layout.scss';
 import './Themes.scss';
 import Sidebar from '../Sidebar/Sidebar.js';
-import { Outlet } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import Home from '../Home/Home'
+import Works from '../Works/Works'
+import Posts from '../Posts/Posts'
+import Contact from '../Contact/Contact'
 
 export default function Layout() {
     return (
         <div className="container">
             <Sidebar />
             <div className="page dark">
-                <Outlet />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/works" element={<Works />} />
+                    <Route path="/posts" element={<Posts />} />
+                    <Route path="/contact" element={<Contact />} />
+                </Routes>
             </div>
         </div>
     );
