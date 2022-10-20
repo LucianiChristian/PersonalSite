bf25ac632297ad36bf73d4af41a7c56725f287bc
Christian Luciani <luciani_christian@yahoo.com>
2022-10-20 13:00:55 -0500

    Styling home page in progress

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import "./Home.scss";
+import personalPicture from "./personalPicture.jpg";
 
 export default function Home() {
     return (
-        <p className="test">Home Page</p>
+        <div className="home">
+            <div className="top">
+                <h1>Hello, I'm Christian Luciani.</h1>
+                <div className="personalPictureContainer">
+                    <div className="personalPicture"></div>
+                </div>
+                <h1>A web developer from the U.S.</h1>
+            </div>
+            <div className="middle">
+
+            </div>
+            <div className="bottom">
+
+            </div>
+        </div>
     )
 }
