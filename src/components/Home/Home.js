b6360c43b94849d785386ee5a6386729d84c800a
Christian Luciani <luciani_christian@yahoo.com>
2022-10-21 18:40:47 -0500

    Insert placeholder body content on Home Page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -12,7 +12,8 @@ export default function Home() {
                 <h2>A web developer from the U.S.</h2>
             </div>
             <div className="middle">
-
+                <p className="bio-A">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam quisquam dicta error laboriosam laborum voluptatibus tempora, minima ullam unde accusamus inventore voluptatem blanditiis esse adipisci? Aliquam, tenetur vitae. Provident, sapiente.</p>
+                <p className="bio-B">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat in ipsam illum eveniet quod? Nesciunt architecto doloribus magnam deleniti ullam iste reiciendis inventore dolorem praesentium officia, ut necessitatibus, doloremque blanditiis.</p>
             </div>
             <div className="bottom">
 
