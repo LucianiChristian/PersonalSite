d70d9c7cf50e921550c8467a1d756623c6b54386
Christian Luciani <luciani_christian@yahoo.com>
2022-10-26 17:22:15 -0500

    Establish email.js code, need to fix error.

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import './Contact.scss';
+import emailjs from '@emailjs/browser';
 
 export default function Contact() {
+    const refForm = React.useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm(
+            'gmail',
+            'contact_form',
+            refForm.current,
+            'rFUFpdGG4sJyiKsHQ'
+        )
+        .then(() => {
+            alert("Message successfully sent!");
+            window.location.reload(false);
+        },
+        () => {
+            alert("Failed to send the message, please try again.");
+        });
+    }
+
     return (
         <div className="contact">
             <h1>Contact Me</h1>
             <p>I am interested in professional or freelance opportunities. If you have any questions of requests, feel free to use the form below.</p>
             <div className="contactForm">
-                <form>
+                <form ref={refForm} onSubmit={sendEmail}>
                     <ul>
                         <li>
                             <input type="text" name="name" placeholder="Name" required/>
@@ -22,7 +43,7 @@ export default function Contact() {
                             <textarea name="message" placeholder="Message" required/>
                         </li>       
                     </ul>
-                    <button>send</button>
+                    <button type="submit">send</button>
                 </form>
             </div>
         </div>
