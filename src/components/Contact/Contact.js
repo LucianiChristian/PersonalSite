9def92dd6ac279d0ba723606b0e770a8f91bf630
Christian Luciani <luciani_christian@yahoo.com>
2022-10-26 15:43:15 -0500

    Create basic form, still requies styling and responsiveness.

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,6 +3,27 @@ import './Contact.scss';
 
 export default function Contact() {
     return (
-        <p>Contact Page</p>
+        <div className="contact">
+            <p>Contact Page</p>
+            <div className="contactForm">
+                <form>
+                    <ul>
+                        <li>
+                            <input type="text" name="name" placeholder="Name"/>
+                        </li>
+                        <li>
+                            <input type="email" name="email" placeholder="Email"/>
+                        </li>
+                        <li>
+                            <input type="text" name="subject" placeholder="Subject"/>
+                        </li>
+                        <li>
+                            <textarea name="message" placeholder="Message"/>
+                        </li>       
+                    </ul>
+                    <button>send</button>
+                </form>
+            </div>
+        </div>
     )
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import "./Home.scss";
+import 'animate.css';
 
 export default function Home() {
     return (
-        <div className="home">
+        <div className="home animate__animated animate__fadeIn">
             <div className="top">
                 <h2>Hello, I'm Christian Luciani.</h2>
                 <div className="personalPictureContainer">
@@ -12,7 +13,11 @@ export default function Home() {
                 <h2>A web developer from the U.S.</h2>
             </div>
             <div className="middle">
-                <p className="bio-A">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam quisquam dicta error laboriosam laborum voluptatibus tempora, minima ullam unde accusamus inventore voluptatem blanditiis esse adipisci? Aliquam, tenetur vitae. Provident, sapiente.</p>
+                <p className="bio-A">As an undergraduate I have been exploring the field of Computer Science with an ever-growing interest. Although I
+                    have gained a wide-range of knowledge from my courses, the true value and joy has come through self-directed
+                    study and 'learning how to learn'. In the last year of my degree I have spent just as much time on my own projects
+                    as I have handling classes with great results.
+                </p>
                 <p className="bio-B">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat in ipsam illum eveniet quod? Nesciunt architecto doloribus magnam deleniti ullam iste reiciendis inventore dolorem praesentium officia, ut necessitatibus, doloremque blanditiis.</p>
             </div>
             <div className="bottom">
