c91da0a9972ec06ed7df7b6346c49253d639d32b
Christian Luciani <luciani_christian@yahoo.com>
2022-10-26 16:59:53 -0500

    Initialized Email.js library and script

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,21 +4,22 @@ import './Contact.scss';
 export default function Contact() {
     return (
         <div className="contact">
-            <p>Contact Page</p>
+            <h1>Contact Me</h1>
+            <p>I am interested in professional or freelance opportunities. If you have any questions of requests, feel free to use the form below.</p>
             <div className="contactForm">
                 <form>
                     <ul>
                         <li>
-                            <input type="text" name="name" placeholder="Name"/>
+                            <input type="text" name="name" placeholder="Name" required/>
                         </li>
                         <li>
-                            <input type="email" name="email" placeholder="Email"/>
+                            <input type="email" name="email" placeholder="Email" required/>
                         </li>
                         <li>
-                            <input type="text" name="subject" placeholder="Subject"/>
+                            <input type="text" name="subject" placeholder="Subject" required/>
                         </li>
                         <li>
-                            <textarea name="message" placeholder="Message"/>
+                            <textarea name="message" placeholder="Message" required/>
                         </li>       
                     </ul>
                     <button>send</button>
