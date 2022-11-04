228ec49816c00cc3f0c307addc7cdee056c08b2e
Christian Luciani <luciani_christian@yahoo.com>
2022-11-04 13:19:35 -0500

    Start mapping from props to Project Card

---
+++ src/components/Works/ProjectData.js
@@ -0,0 +1,12 @@
+const projects = [
+    {
+        title: "Tic-Tac-Toe",
+        stack: "HTML CSS JS",
+        image: "./pathy",
+        description: "Implemented MVC design pattern to ...",
+        repoLink: "https://...",
+        demoLink: "https://...", 
+    }
+];
+
+export default projects;
--- src/components/Works/Works.js
+++ src/components/Works/Works.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './Works.scss';
 import ProjectCard from './ProjectCard';
+import projectData from './ProjectData';
 
 export default function Works() {
     return (
         <div className="works animate__animated animate__fadeIn">
             <h1>Projects</h1>
             <div className="projects">
-                <ProjectCard />
+                {projectData.map(project => (
+                    <ProjectCard projectData = {project}/>
+                ))}
+
                 <ProjectCard />
             </div>
         </div>
