eae4c72daccea6059385a28280f72c1e64401f90
Christian Luciani <luciani_christian@yahoo.com>
2022-11-05 11:01:10 -0500

    Populated all Project Card data

--- src/components/Works/ProjectData.js
+++ src/components/Works/ProjectData.js
@@ -2,11 +2,43 @@ const projects = [
     {
         title: "Tic-Tac-Toe",
         stack: "HTML CSS JS",
-        image: require("./ProjectImages/TicTacToe.png"),
-        description: "A tic-tac-toe game in Vanilla JS. Play against a friend or an AI.",
+        image: require("./ProjectImages/ticTacToe.png"),
+        description: "Tic-tac-toe game in Vanilla JS. Play against a friend or an AI.",
         repoLink: "https://github.com/LucianiChristian/tic-tac-toe",
         demoLink: "https://lucianichristian.github.io/tic-tac-toe/", 
-    }
+    },
+    {
+        title: "Project Manager",
+        stack: "HTML CSS JS",
+        image: require("./ProjectImages/projectManager.png"),
+        description: "Project Management application making use of local storage to store persistent user data.",
+        repoLink: "https://github.com/LucianiChristian/ProjectManager",
+        demoLink: "https://lucianichristian.github.io/to-do-list/", 
+    },
+    {
+        title: "Calculator",
+        stack: "HTML CSS JS",
+        image: require("./ProjectImages/calculator.png"),
+        description: "Calculator capable of all standard functionality.",
+        repoLink: "https://github.com/LucianiChristian/calculator",
+        demoLink: "https://lucianichristian.github.io/calculator/", 
+    },
+    {
+        title: "Book Log",
+        stack: "HTML CSS JS",
+        image: require("./ProjectImages/bookLog.png"),
+        description: "Book logging application. Tracks completions by color and progress bar.",
+        repoLink: "https://github.com/LucianiChristian/Library",
+        demoLink: "https://lucianichristian.github.io/Library/", 
+    },
+    {
+        title: "Etch-A-Sketch",
+        stack: "HTML CSS JS",
+        image: require("./ProjectImages/etchASketch.png"),
+        description: "Book logging application. Tracks completions by color and progress bar.",
+        repoLink: "https://github.com/LucianiChristian/etch-a-sketch",
+        demoLink: "https://lucianichristian.github.io/etch-a-sketch/", 
+    },
 ];
 
 export default projects;
