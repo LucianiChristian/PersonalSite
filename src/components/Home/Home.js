2f32d791830a807fe935d0f277464abc29692508
Christian Luciani <luciani_christian@yahoo.com>
2022-11-18 21:10:44 -0600

    Check format and punctuation on Home Page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -14,16 +14,15 @@ export default function Home() {
             </div>
             <div className="middle">
                 <p className="bio-A">
-                    As a fresh graduate in Computer Science I am thrilled to continue my journey into software development. 
-                    Although I have gained 
-                    a wide range of knowledge from my courses, achieving a 3.9 GPA in my major, the true
+                    As a fresh graduate in Computer Science, I am thrilled to continue my journey into software development. 
+                    Although I have gained a wide range of knowledge from my courses, achieving a 3.9 GPA in my major, the true
                     reward has been cultivating a deep interest in the art of learning and all the valuable things I discover about 
                     myself in the process.
                 </p>
                 <p className="bio-B">
                     Outside of coding, I like to spend my time on
                     guitar, which I have been playing for 14 years, exploring new books and concepts, and meditation, which has informed how I
-                    approach everything in day to day life. 
+                    approach everything in day-to-day life. 
                 </p>
             </div>
             <div className="bottom">
